5078e817480fbfc3e8d5b48f5fb3c5cfa36a8131
Jacaranda Perez <jacaranda.perez@gmail.com>
2021-05-18 09:45:20 +0200

    adding some files

---
+++ src/main/java/com/bibliotek/BibliotekApplication.java
@@ -0,0 +1,13 @@
+package com.bibliotek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bibliotek/BibliotekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliotek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
