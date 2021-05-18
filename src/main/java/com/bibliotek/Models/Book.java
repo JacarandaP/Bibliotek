57f14297000958aff98a0810001e1b35e1999f16
miwgu <miwa.guhres@hotmail.com>
2021-05-18 11:26:17 +0200

    Add models

--- src/main/java/com/bibliotek/Models/Book.java
+++ src/main/java/com/bibliotek/Models/Book.java
@@ -1,10 +1,38 @@
 package com.bibliotek.Models;
 
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+
 /**
  * Created by Jacaranda Perez
  * Date: 2021-05-18
  * Project: Bibliotek
  */
 
+@Getter
+@Setter
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue (strategy= GenerationType.IDENTITY)
+    private long id; //Uniq id
+    private String title;
+    private String author;
+    private String publisher;
+
+    @OneToOne
+    @JoinColumn(name="categoryId")
+    private Category category;
+
+    public Book(long id, String title, String author, String publisher, Category category) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.category = category;
+    }
+
+    public Book(){}
 }
---
+++ src/main/java/com/bibliotek/Models/Category.java
@@ -0,0 +1,29 @@
+package com.bibliotek.Models;
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+
+/**
+ * Created by Miwa Guhrés
+ * Date: 2021-05-18
+ * Time: 10:50
+ * Project: Bibliotek
+ * Copyright: MIT
+ */
+@Getter
+@Setter
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    private  String name;
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Category(){}
+}
