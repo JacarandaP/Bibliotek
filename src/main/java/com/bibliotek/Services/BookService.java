857be48f384ea2331732896b1adad38de8c76815
Vilma Kaijser <vilma.kaijser@gmail.com>
2021-05-20 09:24:26 +0200

    added BbookService class

---
+++ src/main/java/com/bibliotek/Services/BookService.java
@@ -0,0 +1,26 @@
+package com.bibliotek.Services;
+
+import com.bibliotek.Models.Book;
+import com.bibliotek.Repositories.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by Vilma Couturier Kaijser
+ * Date: 2021-05-20
+ * Project: Bibliotek
+ * Copyright: MIT
+ */
+
+@RequiredArgsConstructor
+@Service
+public class BookService {
+
+    private final BookRepository repository;
+
+    public Iterable<Book> getBooks(){
+        return repository.findAll();
+    }
+}
