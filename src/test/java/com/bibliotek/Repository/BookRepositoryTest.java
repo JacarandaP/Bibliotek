d51f0b90c55faff49b728f29b6e8217df7c69c8a
Vilma Kaijser <vilma.kaijser@gmail.com>
2021-05-25 10:11:58 +0200

    ci/cd

--- src/test/java/com/bibliotek/Repository/BookRepositoryTest.java
+++ src/test/java/com/bibliotek/Repository/BookRepositoryTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Arrays;
 import java.util.List;
